640974a3eda4cd2f22f38ecf5d7f0176193adfe9
ULOKO JOHN IGBO <jay-i@users.noreply.github.com>
2019-02-09 00:03:39 +0100

    Add files via upload

---
+++ primeNumber.php
@@ -0,0 +1,41 @@
+<?php
+if(isset($_POST['submit'])){
+	$num = $_POST['num'];
+	
+	for($j = 2; $j <= $num; $j++)
+	{
+	for($k = 2; $k < $j; $k++)
+	{
+	if($j%$k ==0)
+	{
+	break;
+	}
+	}
+	if($k == $j)
+		echo "Prime Number : $j <br />";
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Prime Number</title>
+</head>
+<body>
+<table>
+<form name="prime" method="post">
+<h3>Program to calculate Prime Number</h3>
+<tr>
+<td>Enter Number :</td>
+<td><input type="text" name="num" required></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" value="Check" name="submit" /></td>
+</tr>
+</form>
+</table>
+</body>
+</html>
